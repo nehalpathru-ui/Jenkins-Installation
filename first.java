4615998b9c8097bc90d1db8b531b93aba93ea4c0
nehalpathru-ui <nehalpathru@gmail.com>
2026-08-24 19:49:36 +0530

    first.java

---
+++ first.java
@@ -0,0 +1,8 @@
+echo "Cloning repo"
+git clone https://github.com/
+echo "Changing the directory path"
+cd Java-Learning/First-Program/
+echo "Compiling the java program"
+javac First.java
+echo "Executing the java compiled file"
+java First.java
