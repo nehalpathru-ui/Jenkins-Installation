688f248b7bafcae29cd0f5bc67ba787f50c6347e
nehalpathru-ui <nehalpathru@gmail.com>
2026-08-24 19:51:57 +0530

    Update first.java

--- first.java
+++ first.java
@@ -1,8 +1,7 @@
-echo "Cloning repo"
-git clone https://github.com/
-echo "Changing the directory path"
-cd Java-Learning/First-Program/
-echo "Compiling the java program"
-javac First.java
-echo "Executing the java compiled file"
-java First.java
+// Question: Write a Java program that prints "Hello, World!" to the console.
+
+public class First {
+public static void main(String args[l) {
+System.out.println("Hello, World!");
+                                    }
+                  }
